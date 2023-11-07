58354f2c2b369ee2c9def6a7fa1dbb7eea330d6e
Kutlák Tamás <kutlak.tamas@econsult.hu>
2023-11-07 13:05:14 +0100

    finish save

--- src/main/java/com/econsult/formgenerator/util/FormCollectionUpdates.java
+++ src/main/java/com/econsult/formgenerator/util/FormCollectionUpdates.java
@@ -22,7 +22,7 @@ public class FormCollectionUpdates {
         form.getGroups().addAll(updatedGroups);
     }
 
-    public static void updateFormProperties(Form newForm, Form form) {
+    private static void updateFormProperties(Form newForm, Form form) {
         List<FormProperty> updatedProperties = newForm
                 .getPropertyList()
                 .stream()
@@ -33,20 +33,21 @@ public class FormCollectionUpdates {
         form.getPropertyList().addAll(updatedProperties);
     }
 
-    public static void updateFields(Group group){
+    private static void updateFields(Group group){
         List<Field> fields = group
                 .getFields()
                 .stream()
                 .peek(field -> field.setGroup(group))
+                .peek(FormCollectionUpdates::updateChoices)
                 .toList();
 
-        updateFieldProperties(group);
+        updateGroupProperties(group);
 
         group.getFields().clear();
         group.getFields().addAll(fields);
     }
 
-    public static void updateFieldProperties(Group group){
+    private static void updateGroupProperties(Group group){
         List<GroupProperty> updatedProperties = group
                 .getPropertyList()
                 .stream()
@@ -55,5 +56,49 @@ public class FormCollectionUpdates {
 
         group.getPropertyList().clear();
         group.getPropertyList().addAll(updatedProperties);
+    }
+
+    private static void updateChoices(Field field) {
+        List<Choice> choices = field
+                .getChoices()
+                .stream()
+                .peek(choice -> choice.setField(field))
+                .toList();
+
+        updateFieldProperties(field);
+
+        field.getChoices().clear();
+        field.getChoices().addAll(choices);
+
+        choices = field
+                .getChoices()
+                .stream()
+                .peek(FormCollectionUpdates::updateChoiceProperties)
+                .toList();
+
+        field.getChoices().clear();
+        field.getChoices().addAll(choices);
+    }
+
+    private static void updateFieldProperties(Field field) {
+        List<FieldProperty> updatedProperties = field
+                .getPropertyList()
+                .stream()
+                .peek(property -> property.setField(field))
+                .toList();
+
+        field.getPropertyList().clear();
+        field.getPropertyList().addAll(updatedProperties);
+    }
+
+    private static void updateChoiceProperties(Choice choice) {
+        List<ChoiceProperty> updatedProperties = choice
+                .getPropertyList()
+                .stream()
+                .peek(property -> property.setChoice(choice))
+                .toList();
+
+        choice.getPropertyList().clear();
+        choice.getPropertyList().addAll(updatedProperties);
     }
 }
